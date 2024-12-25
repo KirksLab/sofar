9fcb4e120a7f52ce293735ae8649909cf45bb44b
KirksLab <ineteligencia.ai@gmail.com>
2024-12-25 13:11:05 -0300

    Terceiro commit

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const bodyHtml = document.getElementById("bodyHtml");
+const listButton = document.getElementById("listButton");
 const sectionCaution = document.getElementById("sectionCaution");
 const callContact = document.getElementById("contact");
 const exitButton = document.getElementById("exit");
+const buttonCloseNav = document.getElementById("closeMobileNav");
+const MobileNavHtml = document.getElementById("sectionMobileNav");
+const callContactMobile = document.getElementById("contactMobile");
+
+function showMobileNav () {
+    MobileNavHtml.style.display = "flex";
+    bodyHtml.style.overflow = "hidden";
+}
+
+function closeMobileNav () {
+    MobileNavHtml.style.display = "none";
+    bodyHtml.style.overflow = "scroll";
+}
 
 function warnReader () {
     sectionCaution.style.display = "flex";
@@ -12,5 +26,8 @@ function goodBye () {
     window.close();
 }
 
+listButton.addEventListener("click", showMobileNav);
 callContact.addEventListener("click", warnReader);
+buttonCloseNav.addEventListener("click", closeMobileNav);
 exitButton.addEventListener("click", goodBye);
+callContactMobile.addEventListener("click", warnReader);
